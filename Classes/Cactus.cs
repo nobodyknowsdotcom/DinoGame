a9e27ff1b47be148b03dc4a1af2fb3082e677a99
nobodyzhere <myclosestisa@gmail.com>
2021-05-29 23:07:21 +0500

    Added Bird, Cactus, Road, Transform & GameController classes.
    Bird, Cactus & Road classes have DrawSprite method and his own sprite in spritesheet.
    
    Added Bird, Cactus and Road basic drawing logic in GameController.cs

---
+++ Classes/Cactus.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinoGame.Classes
+{
+    public class Cactus
+    {
+        public Transform transform;
+        int cactusImageXCoordinate = 0;
+
+        public Cactus(PointF pos, Size size)
+        {
+            transform = new Transform(pos, size);
+            ChooseRandomPic();
+        }
+
+        public void ChooseRandomPic()
+        {
+            Random randomGenegator = new Random();
+            int randomNumber = randomGenegator.Next(0, 4);
+            switch (randomNumber)
+            {
+                case 0:
+                    cactusImageXCoordinate = 804;
+                    break;
+                case 1:
+                    cactusImageXCoordinate = 754;
+                    break;
+                case 2:
+                    cactusImageXCoordinate = 704;
+                    break;
+                case 3:
+                    cactusImageXCoordinate = 654;
+                    break;
+            }
+        }
+
+        public void DrawSprite(Graphics g)
+        {
+            g.DrawImage(GameController.spriteSheet,
+                new Rectangle(
+                    new Point((int)transform.position.X, (int)transform.position.Y),
+                    new Size(transform.size.Width, transform.size.Height)),
+                cactusImageXCoordinate, 0, 48, 100, GraphicsUnit.Pixel);
+        }
+    }
+}
---
+++ Classes/Transform.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+
+namespace DinoGame.Classes
+{
+    public class Transform
+    {
+        public PointF position;
+        public Size size;
+
+        public Transform(PointF position, Size size)
+        {
+            this.position = position;
+            this.size = size;
+        }
+    }
+}
